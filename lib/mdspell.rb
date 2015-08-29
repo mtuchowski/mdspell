5e64b8f82e8b8a49a489044b8664c02dbb466a06
Marek Tuchowski <marek@tuchowski.com.pl>
2015-08-30 01:52:31 +0200

    Scan directories recursively for markdown files.

--- lib/mdspell.rb
+++ lib/mdspell.rb
@@ -7,5 +7,17 @@ module MdSpell
   def self.run
     cli = MdSpell::CLI.new
     cli.run
+
+    # Scan directories recursively for markdown files.
+    cli.cli_arguments.each_with_index do |filename, index|
+      if Dir.exist?(filename)
+        cli.cli_arguments[index] = Dir["#{filename}/**/*.md"]
+      end
+    end
+    cli.cli_arguments.flatten!
+
+    cli.cli_arguments.each do |filename|
+      puts "Spell-checking #{filename}..." if Configuration[:verbose]
+    end
   end
 end

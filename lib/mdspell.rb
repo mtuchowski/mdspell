79cab6f993e9af1a3b32d9b1ca2d37e87c164c2e
Marek Tuchowski <marek@tuchowski.com.pl>
2016-04-13 01:30:26 +0200

    Fixed typo. Simplified MdSpell.run logic by introducing check_file.

--- lib/mdspell.rb
+++ lib/mdspell.rb
@@ -9,26 +9,27 @@ require 'rainbow'
 
 # This module holds all the MdSpell code (except mdspell shell command).
 module MdSpell
-  # rubocop:disable Metrics/AbcSize
   def self.run(argv)
     cli = MdSpell::CLI.new
     cli.run argv
+    cli.files.each(&method(:check_file))
 
-    # Spell-check each file.
-    cli.files.each do |filename|
-      spell_checker = SpellChecker.new(filename)
-
-      verbose "Spell-checking #{spell_checker.filename}..."
-
-      spell_checker.typos.each do |typo|
-        error "#{spell_checker.filename}:#{typo.line.location}: #{typo.word}"
-      end
-    end
-
-    exit_if_had_errorss
+    exit_if_had_errors
   end
 
   # Private class methods
+
+  def self.check_file(filename)
+    spell_checker = SpellChecker.new(filename)
+    filename = spell_checker.filename
+
+    verbose "Spell-checking #{filename}..."
+
+    spell_checker.typos.each do |typo|
+      error "#{filename}:#{typo.line.location}: #{typo.word}"
+    end
+  end
+  private_class_method :check_file
 
   def self.verbose(str)
     puts str if Configuration[:verbose]
@@ -41,11 +42,12 @@ module MdSpell
   end
   private_class_method :error
 
-  def self.exit_if_had_errorss
+  def self.exit_if_had_errors
     if @had_errors
       # If exit will be suppressed (line in tests or using at_exit), we need to clean @had_errors
       @had_errors = false
       exit(1)
     end
   end
+  private_class_method :exit_if_had_errors
 end

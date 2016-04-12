80dfdd0e3324d7ee043b1a4747006d705c83fb48
Marek Tuchowski <marek@tuchowski.com.pl>
2016-04-13 01:10:41 +0200

    Added output tests.

--- spec/mdspell/mdspell_spec.rb
+++ spec/mdspell/mdspell_spec.rb
@@ -1,25 +1,53 @@
 describe MdSpell do
   let(:simple) { 'spec/examples/simple.md' }
   let(:with_errors) { 'spec/examples/with_errors.md' }
-  let(:args_without_errors) { [[], [simple]] }
-  let(:args_with_errors) { [[with_errors]] }
 
   it { is_expected.to respond_to :run }
 
   context '#run' do
     context 'if there were no errors' do
       it 'should exit without error' do
-        args_without_errors.each do |argv|
-          expect { subject.run argv }.not_to raise_error
-        end
+        expect { subject.run [] }.not_to raise_error
+        expect { subject.run [simple] }.not_to raise_error
+        expect do
+          allow(STDIN).to receive(:read) { '# Header' }
+          subject.run ['-']
+        end.not_to raise_error
+      end
+
+      it 'should not report anything' do
+        expect { subject.run [] }.not_to output.to_stdout
+        expect { subject.run [simple] }.not_to output.to_stdout
+        expect do
+          allow(STDIN).to receive(:read) { '# Header' }
+          subject.run ['-']
+        end.not_to output.to_stdout
       end
     end
 
     context 'if there were errors' do
+      # rubocop:disable Lint/HandleExceptions:
+      def run_safely(args)
+        subject.run args
+      rescue SystemExit
+        # expected
+      end
+
       it 'should exit with error code' do
-        args_with_errors.each do |argv|
-          expect { subject.run argv }.to raise_error SystemExit
-        end
+        expect { subject.run [with_errors] }.to raise_error SystemExit
+        expect do
+          allow(STDIN).to receive(:read) { 'actualy' }
+          subject.run ['-']
+        end.to raise_error SystemExit
+      end
+
+      it 'should report them' do
+        expect { run_safely [with_errors] }.to output(/mispelled|qiute|actualy|tobe/).to_stdout
+
+        expect do
+          allow(STDIN).to receive(:read) { 'actualy' }
+          run_safely ['-']
+        end.to output(/actualy/).to_stdout
       end
     end
   end
